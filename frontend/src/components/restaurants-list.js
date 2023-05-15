a0dbff0e2356356f1f36da2815e2252aa19cf982
Jean-Daniel Liwanga <jeandaniel.liwanga@gmail.com>
2023-05-15 17:11:39 -0400

    Part 1 for connecting backend function to front end react view

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import RestaurantDataService from "../services/restaurant.js"
+import { Link } from "react-router-dom";
 
 
-function RestaurantsList() {
+const RestaurantsList = props => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchZip, setSearchZip] = useState("");
+  const [searchCuisine, setSearchCuisine] = useState("");
+  const [cuisines, setCuisines] = useState(['All Cuisines']);
+
+  useEffect(() => {
+    retrieveRestaurants();
+    retrieveCuisines();
+  })
+  
+  const onChangeSearchName = e => {
+    const searchName = e.target.value;
+    setSearchName(searchName);
+  }
+
+  const onChangeSearchZip = e => {
+    const searchZip = e.target.value;
+    setSearchZip(searchZip);
+  }
+
+  const onChangeSearchCuisine = e => {
+    const searchCuisine = e.target.value;
+    setSearchCuisine(searchCuisine);
+  }
+
+  const retrieveRestaurants = () => {
+    RestaurantDataService.getAll().then(response => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+    }).catch(e => {
+        console.log(e);
+    });
+  };
+
+  const retrieveCuisines = () => {
+    RestaurantDataService.getCuisines().then(response => {
+        console.log(response.data);
+        setCuisines(["All Cuisines"].concat(response.data));
+    }).catch(e => {
+        console.log(e);
+    });
+  };
+
+  const refreshList = () => {
+    retrieveRestaurants();
+  }
+
   return (
     <div className="App">
-        Hello Worldzzzz.
+        Restaunrant List
     </div>
   );
 }
